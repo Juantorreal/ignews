1ec02469edb37509a31381a56b24ec41fa0927ab
Juan Torreal <juantorreal@gmail.com>
2022-03-15 15:37:52 -0300

    first

---
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,52 @@
+import { useSession, signIn } from 'next-auth/react';
+import Router, { useRouter } from 'next/router';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss';
+
+
+
+interface SubscribeButtonProps {
+    priceId: string;
+}
+
+export function SubscribeButton({priceId} : SubscribeButtonProps) {
+
+    const { data: session } = useSession()
+
+     async function handleSubscribe() {
+    if (!session) {
+    signIn('github')
+    return;
+    }
+if (session.activeSubscription) {
+    Router.push('/posts');
+    return;
+
+}
+
+    try {
+        const response = await api.post('/subscribe')
+
+        const { sessionId } = response.data;
+
+        const stripe = await getStripeJs()
+
+        await stripe.redirectToCheckout({sessionId})
+    } catch (err) {
+     alert(err.message);   
+    }
+ 
+}
+
+return (
+<button type="button"
+className={styles.subscribeButton}
+onClick={handleSubscribe}>
+    Subscribe Now
+</button>
+
+
+);
+
+}
